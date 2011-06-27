0eb5bd1c0bf0853a9972e384782267535f918784
Eric Hutson <ehutson@gmail.com>
2011-06-27 16:50:09 -0400

    Adding default pages and some style

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,15 @@
 module ApplicationHelper
+  
+  def logo
+    logo = image_tag("oncall_logo.png", :alt => "On-Call")
+  end
+  
+  def title
+    base_title = "On-Call"
+    if @title.nil?
+      base_title
+    else
+      "#{base_title} | #{title}"
+    end
+  end
 end
--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -4,4 +4,4 @@
 # If you change this key, all old signed cookies will become invalid!
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-Oncall::Application.config.secret_token = '19e0e23bcdb39c7ca4b74254275afdfc75ad2f2a989a4683012963c70c3ef209c7089a705fc4c8305eb7d73fc1584951b180fe57e1d9d96d7ab1527d6db9833d'
+Oncall::Application.config.secret_token = '0edaaa489e5f9c6294faa056e6708dcb8f3583dc743a6009b812d1b5c8f779d7fb8664830e444d681f2aa1e4d1f9cc923a8a4491aa413a7341c5bd30e22dafef'
